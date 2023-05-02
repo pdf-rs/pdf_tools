cb105ca25bcc89a315093dfcd527d155234aeb8c
Mark Catley <mark@catley.net.nz>
2023-05-02 16:07:30 +1200

    Update dependencies

--- src/bin/pdf2text.rs
+++ src/bin/pdf2text.rs
@@ -1,9 +1,11 @@
-use pdf::file::File;
+use pdf::file::FileOptions;
 use pdf_tools::page_text;
 
 fn main() {
     let input = std::env::args().nth(1).expect("no input file given");
-    let file = File::open(input).expect("failed to read PDF");
+    let file = FileOptions::cached()
+        .open(input)
+        .expect("failed to read PDF");
     for (page_nr, page) in file.pages().enumerate() {
         if let Ok(page) = page {
             println!("=== PAGE {} ===\n", page_nr);
